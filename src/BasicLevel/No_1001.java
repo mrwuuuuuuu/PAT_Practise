5e2655f71aa06fc1c1856fbbd41aaa119c15aad3
lenovo <lenovo@lenovo-PC>
2015-08-26 13:28:08 +0800

    害死人不偿命的(3n+1)猜想 (15)

---
+++ src/BasicLevel/No_1001.java
@@ -0,0 +1,25 @@
+package BasicLevel;
+
+import java.util.Scanner;
+
+public class No_1001 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		int count=0;
+		while(n!=1){
+			if(n%2==0){
+				n/=2;
+				System.out.print(n+" ");
+			}
+			else{
+				n=(3*n+1)/2;
+				System.out.print(n+" ");
+			}
+			count++;
+		}
+		System.out.println(":"+count);
+	}
+}
+
